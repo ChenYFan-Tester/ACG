21cd08a9b518d9208ac9d037954e71264bf32cae
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2021-02-24 08:26:18 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 var total = 279;
 var masonry = document.getElementById("masonry");
 let prefix = "https://cdn.jsdelivr.net/gh/Xecades/ACG/image/";
-prefix = "https://cdn.jsdelivr.net/npm/chenyfan-happypic/";
 (() => {
     for (var i = 1; i < total; i++) {
         masonry.innerHTML += `<div data-fancybox='gallery' class='post-item col-xs-6 col-sm-4 col-md-3 col-lg-2' data-src='${prefix}${i}.webp'><img class='post-item-img lazy' data-original='${prefix}${i}.webp'></div>`;
@@ -10,11 +9,11 @@ prefix = "https://cdn.jsdelivr.net/npm/chenyfan-happypic/";
     for (var i = 1; i <= 32; i++) {for (var j = 1; j < 100; i++) {
         prefix = `https://cdn.jsdelivr.net/npm/chenyfan-happypic@${i}/`;
         masonry.innerHTML += `<div data-fancybox='gallery' class='post-item col-xs-6 col-sm-4 col-md-3 col-lg-2' data-src='${prefix}${j}.jpg'><img class='post-item-img lazy' data-original='${prefix}${j}.jpg'></div>`;
-    }
+    }}
     for (var i = 1; i <= 19; i++) {for (var j = 1; j < 100; i++) {
         prefix = `https://cdn.jsdelivr.net/npm/chenyfan-happypic-sex@${i}/`;
         masonry.innerHTML += `<div data-fancybox='gallery' class='post-item col-xs-6 col-sm-4 col-md-3 col-lg-2' data-src='${prefix}${j}.jpg'><img class='post-item-img lazy' data-original='${prefix}${j}.jpg'></div>`;
-    }
+    }}
     // fancybox3
     $(function () {
         $('[data-fancybox="gallery"]').fancybox({
